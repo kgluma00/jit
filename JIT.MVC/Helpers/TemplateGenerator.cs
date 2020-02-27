87c44cdd24f2c3ab3bd755c798a6c1de1370d1fc
Kristijan Glumac <fightmen007@gmail.com>
2020-02-27 14:52:35 +0100

    Template Generator CSS + Total working hours

--- JIT.MVC/Helpers/TemplateGenerator.cs
+++ JIT.MVC/Helpers/TemplateGenerator.cs
@@ -10,46 +10,56 @@ using System.Threading.Tasks;
 
 namespace JIT.MVC.Helpers
 {
-    public class TemplateGenerator
-    {
+	public class TemplateGenerator
+	{
 
-        public static string GetHTMLString(ICollection<ProjectViewModel> list, string username)
-        {
+		public static string GetHTMLString(ICollection<ProjectViewModel> list, string username)
+		{
+			double sum = 0;
 
-            var sb = new StringBuilder();
-            sb.Append($@"
-                        <html>
-                            <head>
-                            </head>
-                            <body>
-                                <div class='header'><h1>{username} Working Hours</h1></div>
-                                <table align='center'>
-                                    <tr>
-                                        <th>Project Name</th>
-                                        <th>Working Date</th>
-                                        <th>Working Hours</th>
-                                    </tr>");
+			foreach (var element in list)
+			{
+				sum += element.WorkingHours;
+			}
 
-            foreach (var element in list)
-            {
-                sb.AppendFormat(@"<tr>
-                                    <td>{0}</td>
-                                    <td>{1}</td>
-                                    <td>{2}</td>
-                                  </tr>", element.ProjectName, element.WorkingDate, element.WorkingHours);
-            }
+			var sb = new StringBuilder();
+			sb.Append($@"
+						<html>
+							<head>
+								
+							</head>
+							<body>
+								<div class='header'><h1>{username} Working Hours</h1></div>
+								<table align='center'>
+									<tr>
+										<th>Project Name</th>
+										<th>Working Date</th>
+										<th>Working Hours</th>
+									</tr>");
 
-            sb.Append(@"
-                                </table>
-                            </body>
-                        </html>");
+			foreach (var element in list)
+			{
+				sb.AppendFormat(@"<tr>
+									<td>{0}</td>
+									<td>{1}</td>
+									<td>{2}</td>
+								  </tr>", element.ProjectName, element.WorkingDate, element.WorkingHours);
+			}
 
-            return sb.ToString();
-        }
+			sb.Append($@"
+								</table>
+								<div class='row'>
+									<h1>Total hours: {sum}</h1>
+								</div>
+							</body>
+						</html>");
 
-        internal static string GetHTMLString()
-        {
-            throw new NotImplementedException();
-        }
-    }
+			return sb.ToString();
+		}
+
+		internal static string GetHTMLString()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
